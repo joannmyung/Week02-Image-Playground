57b68baf1bced2173baf5421d1b6c9897896648a
joannmyung <123985374+joannmyung@users.noreply.github.com>
2023-02-07 10:19:55 -0500

    Add files via upload

---
+++ week2.playground/Contents.swift
@@ -0,0 +1,30 @@
+
+// composite images from url
+import UIKit
+
+// Read in an image from a url string
+func imageFor(_ str: String) -> UIImage {
+    let url = URL(string: str)
+    let imgData = try? Data(contentsOf: url!)
+    let uiImage = UIImage(data:imgData!)
+    return uiImage!
+}
+
+// profile image
+let u1 = "https://lh3.googleusercontent.com/a/AEdFTp6gdANzhWmT2dZ69WP2UB5edH-_ZitKIkD0Cwks=s80"
+let ui1 = imageFor(u1)
+
+// itp staff
+let u2 = "https://i.ibb.co/QkWvC7g/1024.jpg"
+let ui2 = imageFor(u2)
+
+let sz = CGSize(width: 200, height: 200)
+let renderer = UIGraphicsImageRenderer(size: sz)
+
+let image = renderer.image { (context) in
+    ui1.draw(in: CGRect(x: 0, y: 0, width: 100, height: 100))
+    ui2.draw(in: CGRect(x: 100, y: 0, width: 1024, height: 1024))
+
+}
+
+image
